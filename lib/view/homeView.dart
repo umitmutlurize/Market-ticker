9b46dd32d6f494f4a32a42af6f16451ea7dcc4bf
umitmutlurize <umitmutlurize@gmail.com>
2021-07-15 22:00:00 +0300

    for percentContainer= previous number cannot savable

--- lib/view/homeView.dart
+++ lib/view/homeView.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:marketticker/models/priceModel.dart';
 import 'package:marketticker/services/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeView extends StatefulWidget {
   @override
@@ -11,16 +12,24 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   ApiService apiService = ApiService();
 
-  Future<Data> refreshButton() async {
-    final response = await apiService.getAllData();
-    return Data.fromJson(response);
+
+
+  _incrementCounter(List<String> value) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+  Future<bool> vss = prefs.setStringList("previous-values", value);
+
+    await prefs.setInt('counter', counter);
   }
 
-  @override
-  void initState() {
-    refreshButton();
-    super.initState();
+
+
+  Future<Data> refreshButton() async {
+    final response = await apiService.getAllData();
+
+    var refreshResponse = Data.fromJson(response);
+    return refreshResponse;
   }
+
 
   @override
   Widget build(BuildContext context) {
@@ -33,73 +42,74 @@ class _HomeViewState extends State<HomeView> {
           title: Text('Market ticker'),
           actions: [
             GestureDetector(
-              onTap: () {setState(() {
+              onTap: () {
                 refreshButton();
-              });},
+                setState(() {
+                  apiService.getAllData();
+                });
+              },
               child: Icon(Icons.refresh),
             )
           ],
         ),
         body: Center(
-          child:
-
-               FutureBuilder<Data>(
-                  future: refreshButton(),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      return ListView.builder(
-                        itemCount: snapshot.data!.rates!.length,
-                        itemBuilder: (context, index) => Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              RichText(
-                                text: TextSpan(
-                                    text: snapshot.data!.rates!.keys
-                                        .toList()[index],
-                                    style: TextStyle(color: Colors.white70),
-                                    children: <InlineSpan>[
-                                      TextSpan(
-                                          text: '/EUR',
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.w200,
-                                              color: Colors.white
-                                                  .withOpacity(0.8)))
-                                    ]),
-                              ),
-                              Text(
-                                  snapshot.data!.rates!.values
-                                      .toList()[index]
-                                      .toString(),
-                                  style: TextStyle(
-                                      color: Colors.white, fontSize: 16)),
-                              Container(
-                                alignment: Alignment.center,
-                                height:
-                                    MediaQuery.of(context).size.height * .05,
-                                width: MediaQuery.of(context).size.height * .09,
-                                decoration: BoxDecoration(
-                                  color: Colors.lightGreenAccent,
-                                  borderRadius: BorderRadius.circular(5),
-                                ),
-                                child: Text(
-                                  'Percent %',
-                                  textAlign: TextAlign.center,
-                                ),
-                              )
-                            ],
+          child: FutureBuilder<Data>(
+            future: refreshButton(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return ListView.builder(
+                  itemCount: snapshot.data!.rates!.length,
+                  itemBuilder: (context, index) {
+                    _incrementCounter(snapshot.data!.rates!.values.toList())
+                 return  Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          RichText(
+                            text: TextSpan(
+                                text: snapshot.data!.rates!.keys.toList()[index],
+                                style: TextStyle(color: Colors.white70),
+                                children: <InlineSpan>[
+                                  TextSpan(
+                                      text: '/EUR',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w200,
+                                          color: Colors.white.withOpacity(0.8)))
+                                ]),
                           ),
-                        ),
-                      );
-                    } else if (snapshot.hasError) {
-                      return Text("${snapshot.error}");
-                    }
+                          Text(
+                              snapshot.data!.rates!.values
+                                  .toList()[index]
+                                  .toString(),
+                              style:
+                              TextStyle(color: Colors.white, fontSize: 16)),
+                          Container(
+                            alignment: Alignment.center,
+                            height: MediaQuery.of(context).size.height * .05,
+                            width: MediaQuery.of(context).size.height * .09,
+                            decoration: BoxDecoration(
+                              color: Colors.lightGreenAccent,
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                            child: Text(
+                              'Percent %',
+                              textAlign: TextAlign.center,
+                            ),
+                          )
+                        ],
+                      ),
+                    );
+                  }
+                );
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
 
-                    // By default, show a loading spinner.
-                    return LoadingWidget();
-                  },
-                ),
+              // By default, show a loading spinner.
+              return LoadingWidget();
+            },
+          ),
         ),
       ),
     );
